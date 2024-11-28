024423ffa8fe8a235579e961032b23cd5265a892
Hieu Phan <duchieuphan2k1@gmail.com>
2024-11-28 21:55:07 +0700

    update landing page

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
-HOSTNAME=40.90.250.248
+HOSTNAME=172.179.241.27
 USERNAME=azureuser
-SCRIPT="rm -rf strikingo-landing-page;"
+rm -rf strikingo-landing-page.tar.gz
+tar -czvf strikingo-landing-page.tar.gz *
+SCRIPT="rm -rf strikingo-landing-page; rm -rf strikingo-landing-page.tar.gz;"
 
 ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
-scp -r ../strikingo-landing-page "${USERNAME}@${HOSTNAME}:./"
-RUN_SCRIPT="cd strikingo-landing-page;  cd ./strikingo-landing-page; sudo -S pkill -9 python3; sleep 2; tmux new-session -d -s mysession 'python3 -m http.server 8092'"
+scp -r strikingo-landing-page.tar.gz "${USERNAME}@${HOSTNAME}:./"
+RUN_SCRIPT="tar -xzvf strikingo-landing-page.tar.gz; sudo -S pkill -9 python3; sleep 2; tmux new-session -d -s mysession 'python3 -m http.server 8092'"
 ssh -l ${USERNAME} ${HOSTNAME} "${RUN_SCRIPT}" 
 exit 0
